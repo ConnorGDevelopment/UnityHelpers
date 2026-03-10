368b19fd04d54a608d54371b452e32095b46e7c4
ConnorGDevelopment <connorgdevelopment@gmail.com>
2026-03-10 13:17:32 -0400

    Create LockInspector.cs

---
+++ LockInspector.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+static class EditorMenus
+{
+    [MenuItem("Tools/Toggle Inspector Lock %l")]
+    static void ToggleInspectorLock()
+    {
+        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+        ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+
+
+}
